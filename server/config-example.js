9b4f92d66edf49e3dd217982cf37399ee1f25337
DESKTOP-UQLP1Q2\Łukasz <lukkupis@gmail.com>
2019-10-30 16:14:04 +0100

    add server example config

---
+++ server/config-example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db:
+    'mongodb+srv://<username>:<password>@cluster0-ijwg9.mongodb.net/test?retryWrites=true&w=majority',
+  keySession: 'randomKey',
+  maxAgeSession: 24 * 60 * 60 * 1000
+};
